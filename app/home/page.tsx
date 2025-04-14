b929383759cc7445d0dba1ac13517f1f90b94d40
neonid0 <yunusbozcan@std.iyte.edu.tr>
2025-04-14 22:49:58 +0300

    Added page routes.(just an example for future designs)

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 import Image from 'next/image';
-import LogoCanvas from '@/app/components/logo';
 
 export default function Home() {
     return (
-        <div className="fixed flex flex-col h-full align-middle self-center justify-center">
+        <div className="flex flex-col items-center justify-center min-h-screen space-y-8">
             <h1>Home Page for IYTE Yazilim Society Website</h1>
-            <section className='items-center'>
-                <LogoCanvas />
+            <section className='flex flex-row mx-8 space-x-8 items-center'>
+                <Image className='' src="/images/yazilim.png" alt="IYTE Logo" width={200} height={200} />
                 <p>Software for everyone.</p>
 
             </section>
