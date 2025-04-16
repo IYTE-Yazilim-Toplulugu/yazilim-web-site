09b8d20a90842695d1139aa11b3c6ee0e8c0f7f5
neonid0 <yunusbozcan@std.iyte.edu.tr>
2025-04-16 16:47:39 +0300

    Added new components and hooks.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    allowedDevOrigins: [
+        "10.8.34.199",
+    ]
 };
 
 export default nextConfig;
