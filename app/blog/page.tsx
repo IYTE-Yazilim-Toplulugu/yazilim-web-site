98bfba3f899fe1e4706a6ccef8869c49cdf28b05
Hakan <143508175+KebanFiru@users.noreply.github.com>
2025-06-02 20:41:10 +0300

    Initial commit for blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-import BlogPreview from "@/components/blog-preview";
+
+import { Card, CardContent, CardFooter, CardTitle, CardDescription, CardHeader } from "@/components/ui/card"
+import  Image  from "next/image";
 
 
 
@@ -7,8 +9,127 @@ export default function Blog() {
 
     return (
         <main className="min-h-screen pt-16">
-            <BlogPreview />
+            
+            <div className="flex flex-col space-y-4 m-10 gap-10 h-200">
+                <div className="flex flex-[3] flex ml-12 mr-18 flex-ver border-white border-1 rounded-2xl" >
+                    <div className="flex-[1] w-full rounded-2xl relative">
+                        <Image
+                        src={""}
+                        alt={"abcd"}
+                        fill
+                        className="rounded-2xl"
+                        />
+                    </div>
+                    <div className="flex-[1] w-full">
+                        <Card className=" max-w h-full w-full rounded-2xl  ">
+                                <CardHeader >
+                                    
+                                    <CardTitle>Card Title</CardTitle>
+                                    <CardDescription>This is a description</CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae odio beatae, possimus voluptatum unde voluptatibus 
+                                        eligendi dicta natus, at aliquam itaque autem? Facere culpa, necessitatibus illo impedit obcaecati architecto mollitia..</p>
+                                </CardContent>
+                                <CardFooter className="flex flex-ver">
+                                    <div className="flex-[1]">
+                                    </div>
+                                    <div className="flex flex-[1] justify-end">
+                                        <button>Read more</button>
+                                    </div>         
+                                </CardFooter>
+                            </Card>
+                    </div>
 
+                </div>
+                <div className="flex-[4] flex flex-ver ml-12">
+                    <div className="flex flex-1 items-center">
+                        <Card className="max-w-md h-full w-full rounded-2xl" >
+                            <CardHeader>
+                                <div className="relative w-full h-50">
+                                    
+                                    <Image
+                                        src={""}
+                                        alt={"abcd"}
+                                        fill
+                                    />
+                                </div>
+                                <div>
+                                    <CardTitle>Card Title</CardTitle>
+                                    <CardDescription>This is a description</CardDescription>
+                                </div>
+                            </CardHeader>
+                            <CardContent >
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem est cupiditate nulla eos, labore ea. 
+                                    Odio veritatis natus, vero distinctio eius nulla quaerat animi fuga rerum dolorum, ratione, tenetur pariatur.</p>
+                            </CardContent>
+                            <CardFooter className="flex flex-ver">
+                                <div className="flex-[1]"></div>
+                                <div className="flex flex-[1] justify-end">
+                                    <button>Read more</button>
+                                </div>
+                            </CardFooter>
+                        </Card>
+                    </div>
+                    <div className="flex flex-1 items-center">
+                        <Card className="max-w-md h-full w-full rounded-2xl" >
+                            <CardHeader>
+                                <div className="relative w-full h-50">
+                                    
+                                    <Image
+                                        src={""}
+                                        alt={"abcd"}
+                                        fill
+                                    />
+                                </div>
+                                <div>
+                                    <CardTitle>Card Title</CardTitle>
+                                    <CardDescription>This is a description</CardDescription>
+                                </div>
+                            </CardHeader>
+                            <CardContent >
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem est cupiditate nulla eos, labore ea. 
+                                    Odio veritatis natus, vero distinctio eius nulla quaerat animi fuga rerum dolorum, ratione, tenetur pariatur.</p>
+                            </CardContent>
+                            <CardFooter className="flex flex-ver">
+                                <div className="flex-[1]"></div>
+                                <div className="flex flex-[1] justify-end">
+                                    <button>Read more</button>
+                                </div>
+                            </CardFooter>
+                        </Card>
+                    </div>
+                    <div className="flex flex-1 items-center">
+                         <Card className="max-w-md h-full w-full rounded-2xl" >
+                            <CardHeader>
+                                <div className="relative w-full h-50">
+                                    
+                                    <Image
+                                        src={""}
+                                        alt={"abcd"}
+                                        fill
+                                    />
+                                </div>
+                                <div>
+                                    <CardTitle>Card Title</CardTitle>
+                                    <CardDescription>This is a description</CardDescription>
+                                </div>
+                            </CardHeader>
+                            <CardContent >
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem est cupiditate nulla eos, labore ea. 
+                                    Odio veritatis natus, vero distinctio eius nulla quaerat animi fuga rerum dolorum, ratione, tenetur pariatur.</p>
+                            </CardContent>
+                            <CardFooter className="flex flex-ver">
+                                <div className="flex-[1]"></div>
+                                <div className="flex flex-[1] justify-end">
+                                    <button>Read more</button>
+                                </div>
+                            </CardFooter>
+                        </Card>
+                    </div>
+                </div>
+
+            </div>
         </main>
     )
 }
