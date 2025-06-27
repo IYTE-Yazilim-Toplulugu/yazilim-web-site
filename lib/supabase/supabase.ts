2e24a10842226152045ae192e9220cb889c800c3
etkmlm <furkanmahmutyilmaz@gmail.com>
2025-06-27 03:10:54 +0300

    admin user details, update, base of delete, fixed new admin template html tag in body bug

--- lib/supabase/supabase.ts
+++ lib/supabase/supabase.ts
@@ -1,9 +1,9 @@
-import { createClient } from '@/lib/supabase/client';
+import {createClient} from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY;
 
-//@ts-ignore
+// @ts-ignore
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
